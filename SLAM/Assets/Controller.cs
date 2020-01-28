16e575bd10ba3605e75e39b5068de55496e2ac3c
Winston Liu <winwinstonliu@gmail.com>
2020-01-27 22:56:01 -0800

    added raycast sensors

--- SLAM/Assets/Controller.cs
+++ SLAM/Assets/Controller.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class Controller : MonoBehaviour
 {
-    public float lin_speed = 0.3f;
-    public float ang_speed = 1;
+    private float[] raycast_angle_degrees = { -25, -20, -15, -10, -5, 0, 5, 10, 15, 20, 25 };
+
+    private float lin_speed = 0.1f;
+    private float ang_speed = 0.05f;
+    private float offset = 0.333f;
 
     Rigidbody2D rigidbody;
 
@@ -23,31 +26,65 @@ public class Controller : MonoBehaviour
 
     private void FixedUpdate()
     {
-        float ang = rigidbody.transform.eulerAngles.z;
+        ProcessUserInput();
+
+        CastRays();
+
+    }
+
+    void ProcessUserInput()
+    {
+        float ang_radians = Mathf.Deg2Rad * rigidbody.transform.eulerAngles.z;
         Vector3 pos = transform.position;
 
         if (Input.GetKey(KeyCode.W))
         {
-            pos += lin_speed * new Vector3(Mathf.Cos(Mathf.Deg2Rad * ang), Mathf.Sin(Mathf.Deg2Rad * ang), 0);
+            pos += lin_speed * new Vector3(Mathf.Cos(ang_radians), Mathf.Sin(ang_radians), 0);
         }
 
         if (Input.GetKey(KeyCode.S))
         {
-            pos += -lin_speed * new Vector3(Mathf.Cos(Mathf.Deg2Rad * ang), Mathf.Sin(Mathf.Deg2Rad * ang), 0);
+            pos += -lin_speed * new Vector3(Mathf.Cos(ang_radians), Mathf.Sin(ang_radians), 0);
         }
 
         if (Input.GetKey(KeyCode.A))
         {
-            ang += ang_speed;
+            ang_radians += ang_speed;
         }
 
         if (Input.GetKey(KeyCode.D))
         {
-            ang += -ang_speed;
+            ang_radians += -ang_speed;
         }
 
+        // "Try" to move to [pos] position
         rigidbody.MovePosition(pos);
 
-        rigidbody.MoveRotation(ang);
+        // "Try" to turn to [ang] degrees
+        rigidbody.MoveRotation(Mathf.Rad2Deg * ang_radians);
+    }
+
+    void CastRays()
+    {
+        // Check for current angle
+        float current_ang_radians = Mathf.Deg2Rad * transform.eulerAngles.z;
+        Vector3 direction = transform.right;
+
+        // Find sensor location
+        Vector3 sensor_position = transform.position + offset * direction;
+
+        // Create array to store sensor readings
+        float[] raycast_distances = new float[raycast_angle_degrees.Length];
+
+        for (int i = 0; i < raycast_angle_degrees.Length; i++)
+        {
+            RaycastHit2D hit = Physics2D.Raycast(sensor_position, Quaternion.AngleAxis(raycast_angle_degrees[i], Vector3.back) * direction);
+            if (hit.collider != null)
+            {
+                Debug.DrawLine(sensor_position, hit.point);
+                raycast_distances[i] = hit.distance;
+            }
+        }
+
     }
 }
